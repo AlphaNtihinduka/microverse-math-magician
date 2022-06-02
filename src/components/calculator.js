aaa6c36bad41b9182490019f409b5e9603f07f1a
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-06-02 21:04:24 +0300

    Refactor with hooks

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,59 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-    this.onClick = this.onClick.bind(this);
-  }
+const Calculator = () => {
+  const [state, setState] = useState({ total: 0, next: null, operation: null });
 
-  onClick = (event) => {
-    const result = calculate(this.state, event.target.id);
-    this.setState(result);
+  const onClick = (event) => {
+    const result = calculate(state, event.target.id);
+    setState(result);
   };
 
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <div>
-        <div className="result">
-          <span>{total}</span>
-          <span>{operation}</span>
-          <span>{next}</span>
-        </div>
-        <div className="btn-container">
-          <Button className="top" onClick={this.onClick} id="AC" />
-          <Button className="top" onClick={this.onClick} id="+/-" />
-          <Button className="top" onClick={this.onClick} id="%" />
-          <Button className="right-btn" onClick={this.onClick} id="÷" />
-
-          <Button className="top" onClick={this.onClick} id="7" />
-          <Button className="top" onClick={this.onClick} id="8" />
-          <Button className="top" onClick={this.onClick} id="9" />
-          <Button className="right-btn" onClick={this.onClick} id="x" />
-
-          <Button className="top" onClick={this.onClick} id="4" />
-          <Button className="top" onClick={this.onClick} id="5" />
-          <Button className="top" onClick={this.onClick} id="6" />
-          <Button className="right-btn" onClick={this.onClick} id="-" />
-
-          <Button className="top" onClick={this.onClick} id="1" />
-          <Button className="top" onClick={this.onClick} id="2" />
-          <Button className="top" onClick={this.onClick} id="3" />
-          <Button className="right-btn" onClick={this.onClick} id="+" />
-
-          <Button className="top zero-btn" onClick={this.onClick} id="0" />
-          <Button className="top" onClick={this.onClick} id="." />
-          <Button className="right-btn" onClick={this.onClick} id="=" />
-        </div>
+  return (
+    <div>
+      <div className="result">
+        <span>{state.total}</span>
+        <span>{state.operation}</span>
+        <span>{state.next}</span>
       </div>
-    );
-  }
-}
+      <div className="btn-container">
+        <Button className="top" onClick={onClick} id="AC" />
+        <Button className="top" onClick={onClick} id="+/-" />
+        <Button className="top" onClick={onClick} id="%" />
+        <Button className="right-btn" onClick={onClick} id="÷" />
+
+        <Button className="top" onClick={onClick} id="7" />
+        <Button className="top" onClick={onClick} id="8" />
+        <Button className="top" onClick={onClick} id="9" />
+        <Button className="right-btn" onClick={onClick} id="x" />
+
+        <Button className="top" onClick={onClick} id="4" />
+        <Button className="top" onClick={onClick} id="5" />
+        <Button className="top" onClick={onClick} id="6" />
+        <Button className="right-btn" onClick={onClick} id="-" />
+
+        <Button className="top" onClick={onClick} id="1" />
+        <Button className="top" onClick={onClick} id="2" />
+        <Button className="top" onClick={onClick} id="3" />
+        <Button className="right-btn" onClick={onClick} id="+" />
+
+        <Button className="top zero-btn" onClick={onClick} id="0" />
+        <Button className="top" onClick={onClick} id="." />
+        <Button className="right-btn" onClick={onClick} id="=" />
+      </div>
+    </div>
+  );
+};
+
 export default Calculator;
