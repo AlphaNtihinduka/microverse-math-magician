b94f1f9e2d2255903f37033ef79f99bbca5c040c
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-06-08 22:58:47 +0300

    Add quote page

---
+++ src/components/Quote.js
@@ -0,0 +1,19 @@
+import Linking from './Linking';
+import '../css/quote.css';
+
+const Quote = () => (
+  <>
+    <Linking />
+    <main className="quote-main">
+      <p>
+        We will always have STEM with us. Some things
+        will drop out of the public eye and go away,
+        but there will always be science, engineering,
+        and technology. And there will always,
+        always be mathematics- Stefan Banach, Polish mathematician.
+      </p>
+    </main>
+  </>
+);
+
+export default Quote;
